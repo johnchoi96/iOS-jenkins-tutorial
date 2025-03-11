5609c1434c625925e3f20602da4dbc21fce37af7
John Choi <johnchoi1003@icloud.com>
2025-03-10 22:54:06 -0400

    yes

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by John Choi on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
